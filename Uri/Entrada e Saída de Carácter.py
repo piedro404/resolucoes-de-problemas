1733ec9bad80b791d60e637e2787cf2af2a64dec
Pedro Henrique Martins Borges <pedroplayborges@gmail.com>
2023-10-28 10:03:37 -0300

    upload files

---
+++ Uri/Entrada e Saída de Carácter.py
@@ -0,0 +1,7 @@
+c1 = str(input())
+c2 = str(input())
+c3 = str(input())
+
+print(f"A = {c1}, B = {c2}, C = {c3}")
+print(f"A = {c2}, B = {c3}, C = {c1}")
+print(f"A = {c3}, B = {c1}, C = {c2}")
---
+++ Uri/Entrada e Saída de Vários Tipos.py
@@ -0,0 +1,6 @@
+dados = list(input().split())
+dados[1] = float(dados[1])
+
+print(f"{dados[0]}{dados[1]:.6f}{dados[2]}{dados[3]}")
+print(f"{dados[0]}\t{dados[1]:.6f}\t{dados[2]}\t{dados[3]}")
+print(f"{dados[0]}          {dados[1]:.6f}          {dados[2]}          {dados[3]}")
