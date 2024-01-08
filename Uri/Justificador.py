ba792a526857aec79ba8fefa5e40d1151d9d0a44
Pedro Henrique Martins Borges <pedro.henrique.martins404@gmail.com>
2024-01-07 23:28:42 -0300

    upload files

---
+++ Uri/Justificador.py
@@ -0,0 +1,28 @@
+first = True
+def alinhador(text, lenL):
+    for i in range(len(text)):
+        if text[i] == "":
+            print('')
+        else:
+            print(text[i].rjust(lenL))
+    
+while True:
+    try:
+        lenL = 0
+        texts = []
+        x = int(input())
+        if x == 0:
+            break
+        
+        for _ in range(x):
+            texts.append(str(input()))
+            if len(texts[_]) > lenL:
+                lenL = len(texts[_])
+        
+        if first == False:
+            print()  
+        alinhador(texts, lenL)
+        
+        first = False
+    except EOFError:
+        break
