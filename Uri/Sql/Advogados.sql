72dbd6c8b3ffba9356eec3965a76e4f683c48eae
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-02-08 10:58:45 -0300

    feat: Upload Files SQL

---
+++ Uri/Sql/Advogados.sql
@@ -0,0 +1,22 @@
+WITH max_customers AS (
+    SELECT name, customers_number
+    FROM lawyers
+    ORDER BY customers_number DESC
+    LIMIT 1
+),
+min_customers AS (
+    SELECT name, customers_number
+    FROM lawyers
+    ORDER BY customers_number ASC
+    LIMIT 1
+),
+average_customers AS (
+    SELECT 'Average' AS name, ROUND(AVG(customers_number)) AS customers_number
+    FROM lawyers
+)
+
+SELECT * FROM max_customers
+UNION ALL
+SELECT * FROM min_customers
+UNION ALL
+SELECT * FROM average_customers;
---
+++ Uri/Sql/Máscara de CPF.sql
@@ -0,0 +1,8 @@
+SELECT 
+    CONCAT(
+        SUBSTRING(cpf FROM 1 FOR 3), '.', 
+        SUBSTRING(cpf FROM 4 FOR 3), '.', 
+        SUBSTRING(cpf FROM 7 FOR 3), '-', 
+        SUBSTRING(cpf FROM 10 FOR 2)
+    ) AS CPF
+FROM natural_person;
