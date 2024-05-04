2481e4c336a5e483842aa59499b941a6d8ceadb3
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-05-04 11:54:15 -0300

    feat: :seedling: Rework Question

--- Uri/Strings/Combinador.py
+++ Uri/Strings/Combinador.py
@@ -1,19 +1,22 @@
-def Combinador(comb):
-    result = ''
-    s = sorted(comb, key=lambda x: len(x))
-    tam = len(s[0])
-
-    for x in range(tam):
-        result += comb[0][x] + comb[1][x]
-
-    result += s[1][tam::]
-
-    return result
-    
+def cobinador(dados):
+    newPalavra = []
+    i = 0
+    while True:
+        c = 0
+        if len(dados[0]) > i:
+            newPalavra.append(dados[0][i])
+        else:
+            c +=1
+        if len(dados[1]) > i:
+            newPalavra.append(dados[1][i])
+        else:
+            c +=1
+            
+        if c == 2:
+            break
+        i+=1
+        
+    return "".join(newPalavra)
 
 for _ in range(int(input())):
-    comb = list(map(str, input().split()))
-    print(Combinador(comb))
-
-
-    
+    print(cobinador(list(map(str, input().split()))))
